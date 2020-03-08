16234d722f6a6bed4ae9eed2e8d0f82fdf7a1286
Radoslaw Zaborski <radex171@gmail.com>
2020-03-08 18:52:46 +0100

    new problem with parser in Hero.js [not display webpage]

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,8 @@ class App extends React.Component {
   render() {
     return (
       <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
-        <h2 className={styles.subtitle}>Hello World!</h2>
+        <h1 className={styles.title}>{pageContents.title}</h1>
+        <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
 
         <List {...listData}/>
       </main>
