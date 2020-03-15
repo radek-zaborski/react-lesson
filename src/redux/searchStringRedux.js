6b0dddda197d6807cf430d63b65333efbf044e1b
Radoslaw Zaborski <radex171@gmail.com>
2020-03-15 17:54:31 +0100

    add search function

---
+++ src/redux/searchStringRedux.js
@@ -0,0 +1,24 @@
+// selectors
+export const getSearchString = ({searchString}) => searchString;
+export const countVisibleCards = ({cards, searchString}) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
+export const countAllCards = ({cards}) => cards.length;
+
+// action name creator
+const reducerName = 'search';
+const createActionName = name => `app/${reducerName}/${name}`;
+
+// actions types
+export const CHANGE = createActionName('CHANGE');
+// action creators
+export const createActionChangeSearchString = payload => ({ payload, type: CHANGE });
+
+// reducer
+export default function reducer(statePart = '', action = {}) {
+  switch (action.type) {
+    case CHANGE:
+      return action.payload;
+    default:
+      return statePart;
+  }
+}
+
